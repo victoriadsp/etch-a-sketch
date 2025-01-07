66373bf338a5e9efe9374f742d3a4628fb3cd5f3
Your Name <vvictoria758@gmail.com>
2025-01-07 16:53:18 +0100

    added th e hover effect and fixed the createGrif and squareNumber function

--- java.js
+++ java.js
@@ -1,13 +1,17 @@
 console.log('Check if javacode is running');
 
 
+function createGrid(squareCount) {
+const container = document.querySelector('.container');
+container.innerHTML= '';
+}
 
-// Select the container div
-const container = document.querySelector('.container')
 
 // Create 16x16 = 256 squares
-const gridSize = 16 
+const gridSize = squareCount; 
 const totalSquares= gridSize * gridSize
+
+container.style.width
 
 
 // Loop until all squares are made
@@ -18,11 +22,35 @@ for ( let i= 0; i<totalSquares; i++) {
     square.addEventListener('mouseenter', () => {
         square.style.backgroundColor = 'black';
     });
-    
+}
+
+btn.addEventListener('click',(event) => {
+    console.log('clicked');
+});
+
+function createGrid(squareCount) {
 
 }
 
 
+
+
+
+
+
+
+function squareNumber() {
+    let btn = document.querySelector('#btn');
+    let text;
+    let buttonText = prompt('Please enter the amount of squares you want on your etch-a-sketch: ');
+    if (buttonText == null || buttonText =="") {
+        text = 'No amount of squares detected'
+    };
+    else if (buttonText > 100 ) {
+        text = "Too many squares"
+    };
+    
+}
 
 
 
